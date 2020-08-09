feb7d5da014514674dfc6f8c08fcacaee66c0e53
borvelt <borvelt@gmail.com>
2020-08-10 01:37:42 +0430

    Dockerize (#2)
    * dockerize vendors
    
    * dockerize vehicles app
    
    * dockerize container
    
    * using docker-compose

--- vehicles/vue.config.js
+++ vehicles/vue.config.js
@@ -1,23 +1,23 @@
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 module.exports = {
-  	configureWebpack: config => {
-		config.plugins = config.plugins.concat(
-			new WebpackAssetsManifest({
-				output: 'asset-manifest.json',
-			})
-		);
-	},
-	chainWebpack: config => {
-		config.merge({
-			externals: process.env.VUE_APP_BUILD_MODE === 'test' || process.env.NODE_ENV === 'development' ? [] : {
-				'vue': 'Vue',
-				'vue-router': 'VueRouter',
-			},
-		});
-	},
-	css: {
-		extract: false,
-		sourceMap: true,
-	},
+  configureWebpack: config => {
+    config.plugins = config.plugins.concat(
+      new WebpackAssetsManifest({
+        output: 'asset-manifest.json',
+      })
+    );
+  },
+  chainWebpack: config => {
+    config.merge({
+      externals: process.env.VUE_APP_BUILD_MODE === 'test' || process.env.NODE_ENV === 'development' ? [] : {
+        'vue': 'Vue',
+        'vue-router': 'VueRouter',
+      },
+    });
+  },
+  css: {
+    extract: false,
+    sourceMap: true,
+  },
 };
